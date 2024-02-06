2c5adc6e0b4cbe3fa827ff9937a2d660921a608a
getinthere <getinthere@naver.com>
2024-02-06 15:46:58 +0900

    게시글 상세보기 권한체크

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -20,29 +20,29 @@ public class BoardController {
     private final HttpSession session;
     private final BoardRepository boardRepository;
 
-@PostMapping("/board/save")
-public String save(BoardRequest.SaveDTO requestDTO, HttpServletRequest request) {
-    // 1. 인증 체크
-    User sessionUser = (User) session.getAttribute("sessionUser");
-    if (sessionUser == null) {
-        return "redirect:/loginForm";
+    @PostMapping("/board/save")
+    public String save(BoardRequest.SaveDTO requestDTO, HttpServletRequest request) {
+        // 1. 인증 체크
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            return "redirect:/loginForm";
+        }
+
+        // 2. 바디 데이터 확인 및 유효성 검사
+        System.out.println(requestDTO);
+
+        if (requestDTO.getTitle().length() > 30) {
+            request.setAttribute("status", 400);
+            request.setAttribute("msg", "title의 길이가 30자를 초과해서는 안되요");
+            return "error/40x"; // BadRequest
+        }
+
+        // 3. 모델 위임
+        // insert into board_tb(title, content, user_id, created_at) values(?,?,?, now());
+        boardRepository.save(requestDTO, sessionUser.getId());
+
+        return "redirect:/";
     }
-
-    // 2. 바디 데이터 확인 및 유효성 검사
-    System.out.println(requestDTO);
-
-    if (requestDTO.getTitle().length() > 30) {
-        request.setAttribute("status", 400);
-        request.setAttribute("msg", "title의 길이가 30자를 초과해서는 안되요");
-        return "error/40x"; // BadRequest
-    }
-
-    // 3. 모델 위임
-    // insert into board_tb(title, content, user_id, created_at) values(?,?,?, now());
-    boardRepository.save(requestDTO, sessionUser.getId());
-
-    return "redirect:/";
-}
 
 
     @GetMapping({"/", "/board"})
@@ -71,12 +71,23 @@ public String save(BoardRequest.SaveDTO requestDTO, HttpServletRequest request) 
 
     @GetMapping("/board/{id}")
     public String detail(@PathVariable int id, HttpServletRequest request) {
-        System.out.println("id : " + id);
-
-        // 바디 데이터가 없으면 유효성 검사가 필요없지 ㅎ
+        // 1. 모델 진입 - 상세보기 데이터 가져오기
         BoardResponse.DetailDTO responseDTO = boardRepository.findById(id);
 
+        // 2. 페이지 주인 여부 체크 (board의 userId와 sessionUser의 id를 비교)
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        boolean pageOwner;
+        if (sessionUser == null) {
+            pageOwner = false;
+        } else {
+            int 게시글작성자번호 = responseDTO.getUserId();
+            int 로그인한사람의번호 = sessionUser.getId();
+            pageOwner = 게시글작성자번호 == 로그인한사람의번호;
+        }
+
         request.setAttribute("board", responseDTO);
+        request.setAttribute("pageOwner", pageOwner);
         return "board/detail";
     }
 }
