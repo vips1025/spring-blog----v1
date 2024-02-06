35aaed3cb09b099057a23844deef8cd98c4792c5
getinthere <getinthere@naver.com>
2024-02-06 10:42:23 +0900

    글쓰기 페이지 인증 체크 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,18 +1,22 @@
 package shop.mtcoding.blog.board;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import shop.mtcoding.blog.user.User;
 
+import java.util.HashMap;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class BoardController {
 
+    private final HttpSession session;
     private final BoardRepository boardRepository;
 
     @GetMapping({ "/", "/board" })
@@ -24,8 +28,18 @@ public class BoardController {
         return "index";
     }
 
+    //   /board/saveForm 요청(Get)이 온다
     @GetMapping("/board/saveForm")
     public String saveForm() {
+
+        //   session 영역에 sessionUser 키값에 user 객체 있는지 체크
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        //   값이 null 이면 로그인 페이지로 리다이렉션
+        //   값이 null 이 아니면, /board/saveForm 으로 이동
+        if(sessionUser == null){
+            return "redirect:/loginForm";
+        }
         return "board/saveForm";
     }
 
