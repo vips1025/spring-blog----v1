106f009664623d815abba9d8cd4704509f28cac7
getinthere <getinthere@naver.com>
2024-02-06 11:54:11 +0900

    게시글 쓰기 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import shop.mtcoding.blog.user.User;
 
@@ -19,7 +20,32 @@ public class BoardController {
     private final HttpSession session;
     private final BoardRepository boardRepository;
 
-    @GetMapping({ "/", "/board" })
+@PostMapping("/board/save")
+public String save(BoardRequest.SaveDTO requestDTO, HttpServletRequest request) {
+    // 1. 인증 체크
+    User sessionUser = (User) session.getAttribute("sessionUser");
+    if (sessionUser == null) {
+        return "redirect:/loginForm";
+    }
+
+    // 2. 바디 데이터 확인 및 유효성 검사
+    System.out.println(requestDTO);
+
+    if (requestDTO.getTitle().length() > 30) {
+        request.setAttribute("status", 400);
+        request.setAttribute("msg", "title의 길이가 30자를 초과해서는 안되요");
+        return "error/40x"; // BadRequest
+    }
+
+    // 3. 모델 위임
+    // insert into board_tb(title, content, user_id, created_at) values(?,?,?, now());
+    boardRepository.save(requestDTO, sessionUser.getId());
+
+    return "redirect:/";
+}
+
+
+    @GetMapping({"/", "/board"})
     public String index(HttpServletRequest request) {
 
         List<Board> boardList = boardRepository.findAll();
@@ -37,7 +63,7 @@ public class BoardController {
 
         //   값이 null 이면 로그인 페이지로 리다이렉션
         //   값이 null 이 아니면, /board/saveForm 으로 이동
-        if(sessionUser == null){
+        if (sessionUser == null) {
             return "redirect:/loginForm";
         }
         return "board/saveForm";
@@ -45,7 +71,7 @@ public class BoardController {
 
     @GetMapping("/board/{id}")
     public String detail(@PathVariable int id, HttpServletRequest request) {
-        System.out.println("id : "+id);
+        System.out.println("id : " + id);
 
         // 바디 데이터가 없으면 유효성 검사가 필요없지 ㅎ
         BoardResponse.DetailDTO responseDTO = boardRepository.findById(id);
--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,36 +13,46 @@ import java.util.List;
 public class BoardRepository {
     private final EntityManager em;
 
-    public List<Board> findAll(){
+    public List<Board> findAll() {
         Query query = em.createNativeQuery("select * from board_tb order by id desc", Board.class);
         return query.getResultList();
     }
 
-public BoardResponse.DetailDTO findById(int idx) {
-    Query query = em.createNativeQuery("select b.id, b.title, b.content, b.user_id, u.username from board_tb b inner join user_tb u on b.user_id = u.id where b.id = ?");
-    query.setParameter(1, idx);
+    public BoardResponse.DetailDTO findById(int idx) {
+        Query query = em.createNativeQuery("select b.id, b.title, b.content, b.user_id, u.username from board_tb b inner join user_tb u on b.user_id = u.id where b.id = ?");
+        query.setParameter(1, idx);
 
-    Object[] row = (Object[]) query.getSingleResult();
+        Object[] row = (Object[]) query.getSingleResult();
 
-    Integer id = (Integer) row[0];
-    String title = (String) row[1];
-    String content = (String) row[2];
-    int userId = (Integer) row[3];
-    String username = (String) row[4];
+        Integer id = (Integer) row[0];
+        String title = (String) row[1];
+        String content = (String) row[2];
+        int userId = (Integer) row[3];
+        String username = (String) row[4];
 
-    System.out.println("id : "+id);
-    System.out.println("title : "+title);
-    System.out.println("content : "+content);
-    System.out.println("userId : "+userId);
-    System.out.println("username : "+username);
+        System.out.println("id : " + id);
+        System.out.println("title : " + title);
+        System.out.println("content : " + content);
+        System.out.println("userId : " + userId);
+        System.out.println("username : " + username);
 
-    BoardResponse.DetailDTO responseDTO = new BoardResponse.DetailDTO();
-    responseDTO.setId(id);
-    responseDTO.setTitle(title);
-    responseDTO.setContent(content);
-    responseDTO.setUserId(userId);
-    responseDTO.setUsername(username);
+        BoardResponse.DetailDTO responseDTO = new BoardResponse.DetailDTO();
+        responseDTO.setId(id);
+        responseDTO.setTitle(title);
+        responseDTO.setContent(content);
+        responseDTO.setUserId(userId);
+        responseDTO.setUsername(username);
 
-    return responseDTO;
-}
+        return responseDTO;
+    }
+
+    @Transactional
+    public void save(BoardRequest.SaveDTO requestDTO, int userId) {
+        Query query = em.createNativeQuery("insert into board_tb(title, content, user_id, created_at) values(?,?,?, now())");
+        query.setParameter(1, requestDTO.getTitle());
+        query.setParameter(2, requestDTO.getContent());
+        query.setParameter(3, userId);
+
+        query.executeUpdate();
+    }
 }
