65b23cdc3fa4ceec4c13a55b4930ea92acee3652
getinthere <getinthere@naver.com>
2024-02-16 11:51:12 +0900

    댓글 목록 준비중....

--- src/main/java/shop/mtcoding/blog/board/BoardResponse.java
+++ src/main/java/shop/mtcoding/blog/board/BoardResponse.java
@@ -1,15 +1,40 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import shop.mtcoding.blog.user.User;
+
+import java.util.List;
 
 public class BoardResponse {
 
     @Data
     public static class DetailDTO {
-        private int id;
+        private Integer id;
         private String title;
         private String content;
-        private int userId;
+        private Integer userId; // 게시글 작성자 id
         private String username;
+        private Boolean boardOwner;
+
+        public void isBoardOwner(User sessionUser){
+            if(sessionUser == null) boardOwner = false;
+            else boardOwner = sessionUser.getId() == userId;
+        }
+    }
+
+    @Data
+    public static class ReplyDTO {
+        private Integer id;
+        private Integer userId;
+        private String comment;
+        private String username;
+
+        public ReplyDTO(Object[] ob) {
+            this.id = (Integer) ob[0];
+            this.userId = (Integer) ob[1];
+            this.comment = (String) ob[2];
+            this.username = (String) ob[3];
+        }
     }
 }
--- src/main/java/shop/mtcoding/blog/reply/ReplyRepository.java
+++ src/main/java/shop/mtcoding/blog/reply/ReplyRepository.java
@@ -16,6 +16,18 @@ import java.util.List;
 public class ReplyRepository {
     private final EntityManager em;
 
+public List<BoardResponse.ReplyDTO> findByBoardId(int boardId){
+    String q = """
+            select rt.id, rt.user_id, rt.comment, ut.username from reply_tb rt inner join user_tb ut on rt.user_id = ut.id where rt.board_id = ?
+            """;
+    Query query = em.createNativeQuery(q);
+    query.setParameter(1, boardId);
+
+    List<Object[]> rows = query.getResultList();
+
+    return rows.stream().map(row -> new BoardResponse.ReplyDTO(row)).toList();
+}
+
     @Transactional
     public void save(ReplyRequest.WriteDTO requestDTO, int userId) {
         Query query = em.createNativeQuery("insert into reply_tb(comment, board_id, user_id, created_at) values(?,?,?, now())");
